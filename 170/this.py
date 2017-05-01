6869c61f5b52429ff7a47a4d1f19849d53e9502a
Ahmad <ahmedt2@berkeley.edu>
2017-04-30 17:41:54 -0700

    best_scores

--- 170/this.py
+++ 170/this.py
@@ -1,3 +1,22 @@
+import string
+import time
+import numpy as np
+from itertools import chain, combinations
+import random
+import numpy as np
+import itertools
+# list(itertools.product(*arrays))
+import sys
+import string
+import time
+import numpy as np
+from itertools import chain, combinations
+
+def loadFromFile(_name):
+    with open(_name) as f:
+        content = f.readlines()
+    content = [x.strip("\n") for x in content]
+    return content
 
 def write(prob,probs,_f="here.txt"):
     _max = probs[0]
@@ -10,21 +29,26 @@ def write(prob,probs,_f="here.txt"):
     for i in ks:
         f.write(str(i) + '\n')
 
-def help(_name):
+def help():
     print_me = []
+    i = 0
+    best_scores = loadFromFile("best_scores.txt")
     all_probs=["problem1.in", "problem2.in","problem3.in","problem4.in","problem5.in", "problem6.in","problem7.in", "problem8.in", "problem9.in", "problem10.in", "problem11.in", "problem12.in", "problem13.in", "problem14.in", "problem15.in", "problem16.in", "problem17.in", "problem18.in", "problem19.in", "problem20.in", "problem21.in"]
     for prob in  all_probs:
         file_R = prob.strip(".in") + ".out"
-        file_G = prob.strip(".in") + "_greedy.out"
-        content_R = loadFromFile(file_R)
-        content_G = loadFromFile(file_G)
+        # file_G = prob.strip(".in") + "_greedy.out"
+        try:
+            content_R = loadFromFile(file_R)
+        except:
+            print_me.append(["", best_scores[i]])
+            continue
         score_R = float(content_R[2])   # Pounds
-        score_G = float(content_G[2])   # Dollars
+        score_G = float(best_scores[i])   # Dollars
         if score_G >= score_R:
-            print_me.append[content_G[0],content_G[2]]
+            print_me.append([content_G[0],content_G[2]+" [old]"])
         else:
-            print_me.append[content_R[0],content_R[2]]
+            print_me.append([content_R[0],content_R[2]+" [new]"])
 
     f = open("best_scores.txt", 'w')
     for p in print_me:
-        f.write(p[0], ": ", p[1], " \n")
+        f.write(p[1]+ " \n")
