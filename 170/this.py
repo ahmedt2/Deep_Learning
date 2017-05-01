3f40f98fb9cc5949b56b06813732789d48c8bfc1
Ahmad <ahmedt2@berkeley.edu>
2017-04-30 17:46:35 -0700

    best_scores

--- 170/this.py
+++ 170/this.py
@@ -31,10 +31,11 @@ def write(prob,probs,_f="here.txt"):
 
 def help():
     print_me = []
-    i = 0
+    i = -1
     best_scores = loadFromFile("best_scores.txt")
     all_probs=["problem1.in", "problem2.in","problem3.in","problem4.in","problem5.in", "problem6.in","problem7.in", "problem8.in", "problem9.in", "problem10.in", "problem11.in", "problem12.in", "problem13.in", "problem14.in", "problem15.in", "problem16.in", "problem17.in", "problem18.in", "problem19.in", "problem20.in", "problem21.in"]
     for prob in  all_probs:
+        i+=1
         file_R = prob.strip(".in") + ".out"
         # file_G = prob.strip(".in") + "_greedy.out"
         try:
@@ -45,10 +46,10 @@ def help():
         score_R = float(content_R[2])   # Pounds
         score_G = float(best_scores[i])   # Dollars
         if score_G >= score_R:
-            print_me.append([content_G[0],content_G[2]+" [old]"])
+            print_me.append(["",best_scores[i]+" [old]"])
         else:
             print_me.append([content_R[0],content_R[2]+" [new]"])
 
-    f = open("best_scores.txt", 'w')
+    f = open("best_scores_2.txt", 'w')
     for p in print_me:
         f.write(p[1]+ " \n")
